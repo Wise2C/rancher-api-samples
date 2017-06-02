e3c8338880546bbf14735743949626f1d524cd7d
yunl.zheng <yunl.zheng@wise2c.com>
2017-06-02 14:38:48 +0800

    clean code

--- src/main/java/com/wise2c/samples/RancherClient.java
+++ src/main/java/com/wise2c/samples/RancherClient.java
@@ -38,32 +38,45 @@ public class RancherClient {
         this.secretKey = secretKey;
     }
 
+    /**
+     * 创建Stack实例
+     */
     public Optional<Stack> createStack(Stack stack) throws IOException {
-        PostMethod postMethod = getPostMethod(String.format("%s/stack", endpoint), stack);
-        return Optional.ofNullable(getObjectMapper().readValue(execute(postMethod), Stack.class));
+        return Optional.ofNullable(post(String.format("%s/stack", this.endpoint), stack, Stack.class));
     }
 
+    /**
+     * 在Stack下创建Service
+     */
     public Optional<Service> createService(Service service, String stackId) throws IOException {
         service.setStackId(stackId);
-        PostMethod postMethod = getPostMethod(endpoint + "/service", service);
-        return Optional.ofNullable(getObjectMapper().readValue(execute(postMethod), Service.class));
+        return Optional.ofNullable(post(String.format("%s/service", this.endpoint), service, Service.class));
     }
 
-    private ObjectMapper getObjectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, false);
-        objectMapper.configure(SerializationFeature.FAIL_ON_UNWRAPPED_TYPE_IDENTIFIERS, false);
-        return objectMapper;
+    public void deleteStack(String id) throws IOException {
+        delete(String.format("%s/stacks/%s", this.endpoint, id), Stack.class);
     }
 
-    public void deleteStack(String id) {
-        DeleteMethod deleteMethod = getDeleteMethod(String.format("%s/stacks/%s", endpoint, id));
-        String response = execute(deleteMethod);
-        System.out.println(response);
+    private <T> T get(String url, Class<T> responseClass) throws IOException {
+        GetMethod deleteMethod = new GetMethod(url);
+        return execute(deleteMethod, responseClass);
     }
 
-    private String execute(HttpMethod method) {
+    private <T> T delete(String url, Class<T> responseClass) throws IOException {
+        DeleteMethod deleteMethod = new DeleteMethod(url);
+        return execute(deleteMethod, responseClass);
+    }
+
+    private <T> T post(String url, Object data, Class<T> responseClass) throws IOException {
+        PostMethod method = new PostMethod(url);
+        method.setRequestEntity(getRequestBody(data));
+        return this.execute(method, responseClass);
+    }
+
+    private <T> T execute(HttpMethod method, Class<T> responseClass) {
         try {
+            method.addRequestHeader("Authorization", getAuthorization());
+            method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER, new DefaultHttpMethodRetryHandler(3, false));
             LOGGER.info("request:" + method.getURI().toString());
             int statusCode = new HttpClient().executeMethod(method);
             String responseBody = new String(method.getResponseBody());
@@ -71,35 +84,13 @@ public class RancherClient {
                 throw new RuntimeException(String.format("Some Error Happen statusCode %d response: %s", statusCode, responseBody));
             }
             LOGGER.info("Rancher Response" + responseBody);
-            return responseBody;
+            return getObjectMapper().readValue(responseBody, responseClass);
         } catch (IOException e) {
             e.printStackTrace();
             throw new RuntimeException("Connection to Rancher Failed Please check deploy configuration");
         } finally {
             method.releaseConnection();
         }
-    }
-
-    private HttpMethod getGetMethod(String endpoint) {
-        GetMethod method = new GetMethod(endpoint);
-        method.addRequestHeader("Authorization", getAuthorization());
-        method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER, new DefaultHttpMethodRetryHandler(3, false));
-        return method;
-    }
-
-    private DeleteMethod getDeleteMethod(String uri) {
-        DeleteMethod method = new DeleteMethod(uri);
-        method.addRequestHeader("Authorization", getAuthorization());
-        method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER, new DefaultHttpMethodRetryHandler(3, false));
-        return method;
-    }
-
-    private PostMethod getPostMethod(String uri, Object data) throws UnsupportedEncodingException, JsonProcessingException {
-        PostMethod method = new PostMethod(uri);
-        method.addRequestHeader("Authorization", getAuthorization());
-        method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER, new DefaultHttpMethodRetryHandler(3, false));
-        method.setRequestEntity(getRequestBody(data));
-        return method;
     }
 
     private StringRequestEntity getRequestBody(Object stack) throws JsonProcessingException, UnsupportedEncodingException {
@@ -110,6 +101,13 @@ public class RancherClient {
     private String getAuthorization() {
         byte[] encodedAuth = Base64.encodeBase64((accesskey + ":" + secretKey).getBytes(Charset.forName("US-ASCII")));
         return "Basic " + new String(encodedAuth);
+    }
+
+    private ObjectMapper getObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, false);
+        objectMapper.configure(SerializationFeature.FAIL_ON_UNWRAPPED_TYPE_IDENTIFIERS, false);
+        return objectMapper;
     }
 
 
