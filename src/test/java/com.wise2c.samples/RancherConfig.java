f7e663f1f61d6f9983666461183a69562747e128
yunl.zheng <yunl.zheng@wise2c.com>
2017-06-05 16:34:26 +0800

    update config

--- src/test/java/com.wise2c.samples/RancherConfig.java
+++ src/test/java/com.wise2c.samples/RancherConfig.java
@@ -1,12 +1,10 @@
 package com.wise2c.samples;
 
 public class RancherConfig {
+    
+    public static final String ENDPOINT = "<http://rancher-server/v2-beta>";
+    public static final String SECRET_KEY = "<secret_key>";
+    public static final String ACCESSKEY = "<access_key>";
 
-    //    public static final String ENDPOINT = "http://182.140.210.214:8080/v2-beta/projects/1a5";
-    //    public static final String ACCESSKEY = "3B7DA7433B9A7633FF95";
-    //    public static final String SECRET_KEY = "7WwD3pS6rfYnj2qyn4pLmuXr4PheiKSL96eHfcwh";
-    public static final String ENDPOINT = "http://182.140.210.214:8080/v2-beta";
-    public static final String SECRET_KEY = "H8frTuVYr3h7jApEGTPTeRx6d8HvG7JS8rh1dVXY";
-    public static final String ACCESSKEY = "6366ED550098A089BED4";
 
 }
